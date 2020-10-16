439d24285f792ddc7158ba1b647098da31891fd0
sristi gangwar <69858560+taetae3012@users.noreply.github.com>
2020-10-16 16:38:26 +0530

    Add files via upload
    this is  app.js

---
+++ App.js
@@ -0,0 +1,69 @@
+
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import Addtodo from './components/Addtodo';
+import uuid from 'uuid';
+
+import './App.css';
+
+class App extends Component{
+    state ={
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Meeting with friends',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'dinner with family',
+                completed: false
+            }
+        ]
+            
+        
+    }
+    markComplete =(id) =>{
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id == id){
+                todo.completed = !todo.completed
+            }
+            return todo;
+        }) });
+    }
+
+    delTodo =(id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id!=id)] });
+    }
+
+     addTodo =(title) =>{
+        const newTodo = {
+            id: 4,
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo] });
+     }
+
+    render(){
+        
+        return(
+            <div className="App">
+                <div className="container">
+                <Header />
+                <Addtodo  addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                delTodo={this.delTodo}/>
+                </div>
+            
+            </div>
+        );
+    }
+}
+export default App;
